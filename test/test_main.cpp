ee6f8cb69e454f0a7b18ad19fc89eb6f6a633079
darckore <darckore@gmail.com>
2021-02-15 20:44:44 +0300

    test setting tweaks

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -8,10 +8,20 @@ int main(int argc, char** argv)
 
 TEST(thingy, derpy)
 {
-  EXPECT_EQ(1, 1);
+  EXPECT_EQ(42, 42);
 }
 
 TEST(thingy, herpy)
 {
-  EXPECT_EQ(1, 2);
+  EXPECT_TRUE(42 != 69);
+}
+
+TEST(other_thingy, file)
+{
+  std::ifstream f{ "sample/some_test_file" };
+  EXPECT_TRUE(f);
+
+  std::string file_contents;
+  EXPECT_TRUE(std::getline(f, file_contents));
+  EXPECT_EQ(file_contents, "stub");
 }
